2889c1499266a9fef19cea6fa9eb8ed0ecb9e98a
Jution Candra Kirana <jutionck@gmail.com>
2022-08-31 06:46:01 +0700

    Materi Routing & Navigation - Lazy Load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
-import { SampleRouteModule } from './sample-route/sample-route.module';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
-import { ResumeComponent } from './pages/resume/resume.component';
 
 @NgModule({
   declarations: [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeModule } from './home/home.module';
 import { PageRoutingModule } from './page-routing.module';
 
 @NgModule({
